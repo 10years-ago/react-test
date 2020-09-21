c1c5ab1e1c12a6de3e2e5daca42136216bdb9a91
10years-ago <414578531@qq.com>
2020-09-21 19:41:07 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,16 @@ import {Home} from '../pages/home/home'
 import {User} from '../pages/user/user'
 
 const routes = [
-  { path: '/home', component: Home },
-  { path: '/user', component: User }
+  { 
+    path: '/', 
+    exact: true,
+    component: Home 
+  },
+  {
+     path: '/user',
+     exact: true,
+     component: User 
+  }
 ]
 
 export default routes
